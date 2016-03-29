33237b010501143c365bdd89c325d90196a69410
xmarjun <arjuncmohanan@gmail.com>
2016-03-29 18:50:16 +0530

    Create a new sample class

---
+++ src/Sample.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author root
+ *
+ */
+public class Sample {
+
+	/**
+	 * 
+	 */
+	public Sample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
